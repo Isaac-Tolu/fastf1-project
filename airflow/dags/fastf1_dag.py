0747fc882deaaa4034155b5d070751c96dae318c
Isaac-Tolu <isaactoluwani30@gmail.com>
2023-02-12 20:06:46 +0100

    fixes to conform to airflow's standard

--- airflow/dags/fastf1_dag.py
+++ airflow/dags/fastf1_dag.py
@@ -1,5 +1,5 @@
 import os
-import datetime
+from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
@@ -9,29 +9,33 @@ AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME")
 FASTF1_VENV_NAME = "fastf1-venv"
 YEAR = "{{ execution_date.strftime('%Y') }}"
 
+default_args = {
+    "owner": "airflow",
+    "retries": 1
+}
+
 with DAG(
     dag_id="fastf1_extract_dag",
     description="Extracts data from fastf1 library",
-    start_date=datetime(2018, 0, 0),
+    default_args=default_args,
+    start_date=datetime(2018, 1, 1),
     end_date=datetime(2022, 12, 31),
     schedule_interval="@yearly",
     max_active_runs=1,
     catchup=True,
-    owner="airflow",
-    retries=1
 ) as dag:
 
     begin_task = EmptyOperator(
-        task_id="begin dag"
+        task_id="begin_dag"
     )
 
     extract_task = BashOperator(
-        task_id="extract fastf1 data",
+        task_id="extract_fastf1_data",
         bash_command=f"cd {AIRFLOW_HOME} && source {FASTF1_VENV_NAME}/bin/activate && python extract/fastf1_extract.py {YEAR}"
     )
 
     end_task = EmptyOperator(
-        task_id="end dag"
+        task_id="end_dag"
     )
 
     begin_task >> extract_task >> end_task
