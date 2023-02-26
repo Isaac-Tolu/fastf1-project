38aec757e1e17082f65d708856d72f360ca959db
Isaac-Tolu <isaactoluwani30@gmail.com>
2023-02-26 13:05:32 +0100

    changed program flow

--- extract/fastf1_extract.py
+++ extract/fastf1_extract.py
@@ -1,5 +1,4 @@
-import sys
-from pathlib import Path
+import sys, logging, time
 
 import numpy as np
 import pandas as pd
@@ -10,8 +9,8 @@ from fastf1.core import Lap, Laps, Session, Telemetry
 from fastf1.events import EventSchedule
 from fastf1.core import DataNotLoadedError
 
+import sqlalchemy as sa
 from psycopg2.extensions import register_adapter, AsIs
-from sqlalchemy import create_engine, text
 
 register_adapter(np.int64, AsIs)
 
@@ -22,45 +21,112 @@ YEAR = int(YEAR)
 
 fastf1.Cache.enable_cache("fastf1_cache/")
 
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+handler.setFormatter(format)
+logger.addHandler(handler)
+
 def main():
-    
-    try:
-        engine = create_engine(CONN_STR)
-        conn = engine.connect()
+    engine = sa.create_engine(CONN_STR)
 
-        session_info = get_session_info(YEAR)
-        session_info.to_sql(name="dim_sessions", index=False, con=conn, if_exists='append')
+    session_info = get_session_info(YEAR)
+    upsert_df(session_info, "dim_sessions", engine)
+    logger.info(f"Upserted session information for year {YEAR}")
 
-        for _, sc in session_info.iterrows():
-            session = fastf1.get_session(YEAR, sc["roundnumber"], sc["sessiontype"])
+    for _, sc in session_info.iterrows():
+        round_number = sc["roundnumber"]
+        session_id = sc["sessionid"]
+        session_type = sc["sessiontype"]
 
-            session.load()
+        session = fastf1.get_session(YEAR, round_number, session_type)
 
-            new_drivers = get_new_drivers(session, conn)
-            results_info = get_results(session, sc["sessionid"])
+        session.load()
+        
+        driver_info = get_driver_info(session, session_id)
+        if driver_info is not None:
+            upsert_df(driver_info, "dim_drivers", engine)
+            logger.info(f"Upserted drivers for session {session_id}")
 
-            if new_drivers is not None:
-                new_drivers.to_sql(name="dim_drivers", index=False, con=conn, if_exists='append')
-            if results_info is not None:
-                results_info.to_sql(name="fact_results", index=False, con=conn, if_exists='append')
+        results_info = get_results(session, session_id)
+        if results_info is not None:
+            upsert_df(results_info, "fact_results", engine)
+            logger.info(f"Upserted results for session {session_id}")
 
-            lap_with_weather_info = get_lap_with_weather_info(session, sc["sessionid"])
-            if lap_with_weather_info is not None:
-                lap_with_weather_info.to_sql(name="fact_lap_statistics", index=False, con=engine, if_exists="append")
+        lap_with_weather_info = get_lap_with_weather_info(session, session_id)
+        if lap_with_weather_info is not None:
+            upsert_df(lap_with_weather_info, "fact_lap_statistics", engine, match_columns=["sessionid", "driverid", "lapid"])
+            logger.info(f"Upserted lap and weather statistics for session {session_id}")
 
             lap_gen = session.laps.iterlaps()
             for lap in lap_gen:
-                telemetry_data = get_telemetry_of_lap(lap[1], sc["sessionid"])
-                if telemetry_data is not None:
-                    telemetry_data.to_sql(name="fact_lap_telemetry_statistics", index=False, con=engine, if_exists="append")
-                else:
+                car_data = get_car_data_of_lap(lap[1], session_id)
+                pos_data = get_pos_data_of_lap(lap[1], session_id)
+
+                if (car_data is None) or (pos_data is None):
+                    logger.warning(f"No telemetry data for session {session_id}")
                     break
+                else:
+                    upsert_df(car_data[1], "fact_lap_car_statistics", engine, match_columns=["lapid", "date"])
+                    logger.info(f"Upserted car statistics for lap {car_data[0]}")
 
-    finally:
-        conn.close()
+                    upsert_df(pos_data[1], "fact_lap_position_statistics", engine, match_columns=["lapid", "date"])
+                    logger.info(f"Upserted position statistics for lap {pos_data[0]}")
+
+                time.sleep(1)
+
+            logger.info(f"Upserted car and position statistics for session {session_id}")
+            time.sleep(5)
 
 
-def get_session_info(year:int) -> EventSchedule:
+def upsert_df(df, table_name, engine, match_columns=None):
+    """
+    Perform an "upsert on a PostgreSQL table from a pandas DataFrame.
+
+    Constructs an INSERT ... ON CONFLICT statement, uploads the DataFrame to a 
+    temporary table and then executes the INSERT
+
+    Parameters
+    ----------
+    df: pandas.DataFrame
+        The DataFrame to be upserted.
+    table _name: str
+        The name of the target table.
+    engine: sqlalchemy.engine.Engine
+        The SQLAlchemy engine to use
+    match_columns: list[str], optional
+        A list of the column name(s) on which to match. If omitted, the
+        primary key columns of the target table will be used.  
+    """
+    
+    table_spec = '"' + table_name + '"'
+
+    df_columns = list(df.columns)
+    if not match_columns:
+        insp = sa.inspect(engine)
+        match_columns = insp.get_pk_constraint(table_name, schema=None)[
+            "constrained_columns"
+        ]
+    columns_to_update = [col for col in df_columns if col not in match_columns]
+    insert_col_list = ", ".join([f'"{col_name}"' for col_name in df_columns])
+    stmt = f"INSERT INTO {table_spec} ({insert_col_list})\n"
+    stmt += f"SELECT {insert_col_list} FROM temp_table\n"
+    match_col_list = ", ".join([f'"{col}"' for col in match_columns])
+    stmt += f"ON CONFLICT ({match_col_list}) DO UPDATE SET\n"
+    stmt += ", ".join(
+        [f'"{col}" = EXCLUDED."{col}"' for col in columns_to_update]
+    )
+
+    with engine.begin() as conn:
+        conn.exec_driver_sql("DROP TABLE IF EXISTS temp_table")
+        conn.exec_driver_sql(
+            f"CREATE TEMPORARY TABLE temp_table AS SELECT * FROM {table_spec} WHERE false"
+        )
+        df.to_sql("temp_table", conn, if_exists="append", index=False)
+        conn.exec_driver_sql(stmt)
+
+def get_session_info(year:int):
     """Gets event session data for a particular year.
     """
 
@@ -86,8 +152,8 @@ def get_session_info(year:int) -> EventSchedule:
 
     return schedule_fin
 
-def get_new_drivers(session:Session, conn) -> DataFrame:
-    """Gets the newest drivers not yet added to database"""
+def get_driver_info(session:Session, session_id:str):
+    """Gets drivers for a session"""
     
     try:
         results = session.results.reset_index(names="DriverID")
@@ -102,14 +168,12 @@ def get_new_drivers(session:Session, conn) -> DataFrame:
         driver_info["driverid"] = driver_info["driverid"] + "_" + driver_info["teamname"]
         driver_info["drivernumber"] = driver_info["drivernumber"].astype("int")
 
-        db_drivers = pd.read_sql(text("select * from dim_drivers"), con=conn)
-        new_drivers = pd.concat([driver_info, db_drivers, db_drivers]).drop_duplicates(keep=False)
-
-        return new_drivers
+        return driver_info
     except DataNotLoadedError:
+        logger.warning(f"No driver data for session {session_id}")
         return None
 
-def get_results(session:Session, session_id:str) -> DataFrame:
+def get_results(session:Session, session_id:str):
     """Gets results of the session race"""
     
     try:
@@ -129,9 +193,10 @@ def get_results(session:Session, session_id:str) -> DataFrame:
 
         return results_info
     except DataNotLoadedError:
+        logger.warning(f"No results data for session {session_id}")
         return None
 
-def get_lap_with_weather_info(session:Session, session_id:str) -> Laps:
+def get_lap_with_weather_info(session:Session, session_id:str):
     """Gets lap statistics data.
        Contains weather and other kinds of informaton about each lap"""
 
@@ -158,25 +223,48 @@ def get_lap_with_weather_info(session:Session, session_id:str) -> Laps:
 
         return joined_data
     except DataNotLoadedError:
+        logger.warning(f"No lap statistics info for session {session_id}")
         return None
 
-def get_telemetry_of_lap(lap:Lap, session_id: str) -> Telemetry:
-    """Gets telemetry data for each lap in a particular session"""
+def get_car_data_of_lap(lap:Lap, session_id:str):
+    """Gets car data for a lap in a session"""
 
     try:
-        tel_data = lap.get_telemetry()
-        tel_data["LapID"] = session_id + '_' + str(lap["DriverNumber"]) + '_'  + lap["Team"] + '_' + str(lap["LapNumber"])
+        car_data = lap.get_car_data()
+        lap_id = session_id + '_' + str(lap["DriverNumber"]) + '_' + lap["Team"] + '_' + str(int(lap["LapNumber"]))
+        car_data["LapID"] = lap_id
+        car_data = car_data.loc[:, ~(car_data.columns == 'Source')]
 
-        tel_data.columns = tel_data.columns.str.lower()
+        car_data.columns = car_data.columns.str.lower()
 
-        timedelta_cols = ["sessiontime", "time"]
-        tel_data = convert_td_cols_to_str(tel_data, timedelta_cols)
+        td_cols = ["time", "sessiontime"]
+        car_data = convert_td_cols_to_str(car_data, td_cols)
 
-        return tel_data
+        return lap_id, car_data
     except DataNotLoadedError:
+        logger.warning(f"No car data information for lap {lap_id}")
+        return None
+    
+def get_pos_data_of_lap(lap:Lap, session_id:str):
+    """Gets car position data for a lap in a session"""
+
+    try:
+        pos_data = lap.get_pos_data()
+        lap_id = session_id + '_' + str(lap["DriverNumber"]) + '_' + lap["Team"] + '_' + str(int(lap["LapNumber"]))
+        pos_data["LapID"] = lap_id
+        pos_data = pos_data.loc[:, ~(pos_data.columns == 'Source')]
+
+        pos_data.columns = pos_data.columns.str.lower()
+
+        td_cols = ["time", "sessiontime"]
+        pos_data = convert_td_cols_to_str(pos_data, td_cols)
+
+        return lap_id, pos_data
+    except DataNotLoadedError:
+        logger.warning(f"No position data information for lap {lap_id}")
         return None
 
-def convert_td_cols_to_str(df: DataFrame, td_cols:list) -> DataFrame:
+def convert_td_cols_to_str(df: DataFrame, td_cols:list):
     """Utility function for converting timedelta column to string.
        Postgres cannot read pandas timedelta column"""
 
